db6486cca162fa2746ad9e70bb189f005ea53e7e
Julie Park <jspark721@gmail.com>
2018-07-16 15:50:07 -0700

    add reviews to movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :reviews
+
   devise_for :users
-  resources :movies
+  
+  resources :movies do
+      resources :reviews, except: [:show, :index]
+  end
 
   root 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
